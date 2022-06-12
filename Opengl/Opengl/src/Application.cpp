020849a47a32d62d909cfa4fdf36e3e2a649b794
Rüçhan Adıgüzel <44341663+jceads@users.noreply.github.com>
2022-06-12 23:00:18 +0300

    getting error about init

--- Opengl/Opengl/src/Application.cpp
+++ Opengl/Opengl/src/Application.cpp
@@ -2,10 +2,56 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
+static unsigned int CompileShader (unsigned int type, const std::string& source){
+    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src,nullptr);
+    glCompileShader(id);
+    
+    
+    int results;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &results);
+    if (results == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message =(char*) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "failed to compile shader!" <<(type == GL_VERTEX_SHADER ?"vertex":"fragment")<<"shader" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+
+    }
+
+
+
+    return id;
+}
+
+static int createShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program =  glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER,fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+
+}
+
 int main(void)
 {
     GLFWwindow* window;
 
+    glewExperimental = GL_TRUE;
     /* Initialize the library */
     if (!glfwInit())
         return -1;
@@ -25,10 +71,60 @@ int main(void)
     glfwMakeContextCurrent(window);
 
 
-    if (glewInit != GLEW_OK) 
+    if (glewInit != GLEW_OK) {
+        std::cout << "bune mk";
         std::cout << "Error!" << std::endl;
+    }
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+
+
+
+    
+    //we can call this buffer
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f 
+    };
+
+    //vertex buffer, kaç buffer lazým
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);//buffer created
+    glBindBuffer(GL_ARRAY_BUFFER,buffer);//buffer selected, selecting=binding
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);  //2.parametrede size belirtmemiz lazým
+
+    glEnableVertexAttribArray(0);
+    //2.parametre bir vertexin pozisyonun kaç sayý ile belirtirsin =2,Stride =>https://www.youtube.com/watch?v=x0H--CL2tUI&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=5 videoda anlatýyo
+    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2,0);
+
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location=0)in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position =position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location=0)out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color  =vec4(1.0,0.0,0.0,1.0);\n"
+        "}\n";
+        
+    unsigned int shader = createShader(vertexShader,fragmentShader);
+    glUseProgram(shader);
 
 
     /* Loop until the user closes the window */
@@ -37,14 +133,13 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glColor3f(0.1f, 0.2f, 0.3f);
-        glVertex2f(0.0f, 0.5f);
-        glColor3f(0.4f, 0.5f, 0.8f);
-        glVertex2f(0.5f, -0.5f);
-        glColor3f(0.0f, 0.5f, 0.9f);
-        glEnd();
+        //glBegin(GL_TRIANGLES);
+        //glVertex2f(-0.5f, -0.5f);
+        //glVertex2f( 0.0f, 0.5f );
+        //glVertex2f( 0.5f, -0.5f);
+        //glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
@@ -56,3 +151,4 @@ int main(void)
     return 0;
 }
 
+;
